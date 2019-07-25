6a808072ce56d813c2d27b565ee90a5edeff7dc2
Stacy Johns <stacykjohns@icloud.com>
2019-07-25 23:10:16 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -3,15 +3,15 @@ word.match(/\b[aeiou]/i) ? true : false
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+text.scan(/un\w+ing/i)
 end
 
 def words_five_letters_long(text)
-
+text.scan(/\b[a-z]{5}\b/i)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+text.scan
 end
 
 def valid_phone_number?(phone)
