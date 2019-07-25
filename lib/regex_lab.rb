9ff25a845028f2565fc53ac19c16043f6c518f65
Stacy Johns <stacykjohns@icloud.com>
2019-07-25 23:16:52 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -11,7 +11,7 @@ text.scan(/\b[a-z]{5}\b/i)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-text.scan
+text.scan(/^[A-Z].+\.$/) ? true : false
 end
 
 def valid_phone_number?(phone)
